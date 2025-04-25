b2b76eb60fa463d262e66a5d65bbfa2fecfe66dc
Mykhailo Moskalov <misha@moskalov.com>
2025-04-25 13:26:48 +0200

    Update sale.js

--- src/js/sale.js
+++ src/js/sale.js
@@ -10,7 +10,7 @@ itemWatch.forEach((e, i) => {
 
   function doSwitch() {
     console.log(i+1);
-    mainImg.setAttribute("src", `./img/sale/sale-${i+1}-mob-max.png`);
-    mainImg.setAttribute("srcset", `./img/sale/${i+1}/sale-${i+1}-mob-max.png 1x, ./img/sale/${i+1}/sale-${i+1}-mob@2x-max.png 2x`);
+    mainImg.setAttribute("src", `/img/sale/sale-${i+1}-mob-max.png`);
+    mainImg.setAttribute("srcset", `/img/sale/${i+1}/sale-${i+1}-mob-max.png 1x, /img/sale/${i+1}/sale-${i+1}-mob@2x-max.png 2x`);
   }
 });
